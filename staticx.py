57701436b8b53b2f69aec50f4cafc9c586c1e46d
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-05-27 08:11:33 -0400

    staticx: Parse ldd output

--- staticx.py
+++ staticx.py
@@ -3,6 +3,7 @@ import argparse
 import subprocess
 import os
 import sys
+import re
 
 class AppError(Exception):
     def __init__(self, message, exitcode=2):
@@ -17,6 +18,21 @@ def get_shobj_deps(path):
     except subprocess.CalledProcessError as e:
         raise AppError("'ldd' failed")
 
+    # Example:
+    #	libc.so.6 => /usr/lib64/libc.so.6 (0x00007f42ac010000)
+    #	/lib64/ld-linux-x86-64.so.2 (0x0000557376e75000)
+    pat = re.compile('\t([\w./-]*) (?:=> ([\w./-]*) )?\((0x[0-9a-fA-F]*)\)')
+
+    for line in output.decode('ascii').splitlines():
+        m = pat.match(line)
+        if not m:
+            raise ValueError("Unexpected line in ldd output: " + line)
+        libname  = m.group(1)
+        libpath  = m.group(2)
+        baseaddr = int(m.group(3), 16)
+
+        libpath = libpath or libname
+        yield libpath
 
 
 def parse_args():
@@ -28,7 +44,8 @@ def parse_args():
 def main():
     args = parse_args()
 
-    get_shobj_deps(args.prog)
+    for lib in get_shobj_deps(args.prog):
+        print(lib)
 
 
 if __name__ == '__main__':
