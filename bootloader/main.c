e7bef4c5460e6824dea639eb3baf35d71543409f
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-05-27 08:37:49 -0400

    bootloader: mmap /proc/self/exe

--- bootloader/main.c
+++ bootloader/main.c
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include <error.h>
 #include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 #define verbose_msg(fmt, ...)   fprintf(stderr, fmt, ##__VA_ARGS__)
+
+static void
+extract_archive(const char *destpath)
+{
+    /* Open this ELF file */
+    int fd = open("/proc/self/exe", O_RDONLY);
+    if (fd < 0)
+        error(2, errno, "Failed to open self");
+
+    struct stat st;
+    if (fstat(fd, &st) < 0)
+        error(2, errno, "Failed to stat self");
+
+    void *m = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (m == MAP_FAILED)
+        error(2, errno, "Failed to mmap self");
+}
 
 static char *
 create_tmpdir(void)
@@ -20,6 +41,8 @@ main(int argc, char **argv)
 {
     char *path = create_tmpdir();
     verbose_msg("Temp dir: %s\n", path);
+
+    extract_archive(path);
 
     return 0;
 }
