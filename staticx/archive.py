8220e6a0439c96ea48fdd942b29378226ae797a3
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-06-05 07:51:57 -0400

    staticx: Track libraries added to archive and prevent duplicates

--- staticx/archive.py
+++ staticx/archive.py
@@ -4,17 +4,23 @@ from os.path import basename, islink
 
 from .utils import get_symlink_target
 from .constants import *
+from .errors import *
 
 
 class SxArchive(object):
     def __init__(self, fileobj, mode):
         self.tar = tarfile.open(fileobj=fileobj, mode=mode)
+        self._added_libs = []
 
     def __enter__(self):
         return self
 
     def __exit__(self, *excinfo):
         self.tar.close()
+
+    @property
+    def libraries(self):
+        return iter(self._added_libs)
 
     def add_symlink(self, name, target):
         """Add a symlink to the archive"""
@@ -41,9 +47,12 @@ class SxArchive(object):
         The library will be added with its base name.
         Symlinks will also be added and followed.
         """
-        linklib = path
+
+        if basename(path) in self._added_libs:
+            raise LibExistsError(basename(path))
 
         # 'recursively' step through any symbolic links, generating local links inside the archive
+        linklib = path
         while islink(linklib):
             arcname = basename(linklib)
             linklib = get_symlink_target(linklib)
@@ -51,11 +60,13 @@ class SxArchive(object):
             # add a symlink.  at this point the target probably doesn't exist, but that doesn't matter yet
             logging.info("    Adding Symlink {} => {}".format(arcname, basename(linklib)))
             self.add_symlink(arcname, basename(linklib))
+            self._added_libs.append(arcname)
 
         # left with a real file at this point, add it to the archive.
         arcname = basename(linklib)
         logging.info("    Adding {} as {}".format(linklib, arcname))
         self.tar.add(linklib, arcname=arcname)
+        self._added_libs.append(arcname)
 
     def add_interp_symlink(self, interp):
         """Add symlink for ld.so interpreter"""
