a5d25691f59fafc033a8d70652091a7e84a3db6a
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-06-05 07:55:18 -0400

    staticx: Process PyInstaller archive and add shared lib dependencies
    We use the PyInstaller CArchiveReader API to determine if the given
    application is a PyInstaller archive, and if so, dump out all of the shared
    libraries to a temporary directory. We then run 'ldd' on those libraries,
    and add any libraries not already present in our archive or the PyInstaller
    archive.
    
    Closes #13

--- staticx/api.py
+++ staticx/api.py
@@ -4,7 +4,7 @@
 #
 import subprocess
 import shutil
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, mkdtemp
 import os
 import re
 import logging
@@ -94,6 +94,64 @@ def patch_elf(path, interpreter=None, rpath=None, force_rpath=False):
     tool_patchelf.run(*args)
 
 
+def process_pyinstaller_archive(ar, prog):
+    # See utils/cliutils/archive_viewer.py
+
+    # If PyInstaller is not installed, do nothing
+    try:
+        from PyInstaller.archive.readers import CArchiveReader, NotAnArchiveError
+    except ImportError:
+        return
+
+    # Attempt to open the program as PyInstaller archive
+    try:
+        pyi_ar = CArchiveReader(prog)
+    except:
+        # Silence all PyInstaller exceptions here
+        return
+    logging.info("Opened PyInstaller archive!")
+
+    # Create a temporary directory
+    # TODO PY3: Use tempfile.TemporaryDirectory and cleanup()
+    tmpdir = mkdtemp(prefix='staticx-pyi-')
+
+    try:
+        # Process the archive, looking at all shared libs
+        for n, item in enumerate(pyi_ar.toc.data):
+            (dpos, dlen, ulen, flag, typcd, name) = item
+
+            # Only process binary files
+            # See xformdict in PyInstaller.building.api.PKG
+            if typcd != 'b':
+                continue
+
+            # Extract it to a temporary location
+            x, data = pyi_ar.extract(n)
+            tmppath = os.path.join(tmpdir, name)
+            logging.debug("Extracting to {}".format(tmppath))
+            with open(tmppath, 'wb') as f:
+                f.write(data)
+
+            # TODO: De-duplicate this code with generate_archive()
+            for libpath in get_shobj_deps(tmppath):
+                lib = os.path.basename(libpath)
+
+                if lib.startswith('linux-vdso.so'):
+                    continue
+
+                if lib in ar.libraries:
+                    logging.debug("{} already in staticx archive".format(lib))
+                    continue
+
+                if pyi_ar.toc.find(lib) != -1:
+                    logging.debug("{} already in pyinstaller archive".format(lib))
+                    continue
+
+                ar.add_library(libpath)
+    finally:
+        shutil.rmtree(tmpdir)
+
+
 def generate_archive(prog, interp, extra_libs=None):
     logging.info("Program interpreter: " + interp)
 
@@ -112,6 +170,9 @@ def generate_archive(prog, interp, extra_libs=None):
                 continue
 
             ar.add_library(lib)
+
+
+        process_pyinstaller_archive(ar, prog)
 
     f.flush()
     return f
