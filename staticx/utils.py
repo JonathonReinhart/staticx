f7cf28adedc32e6286e3b3f9b1003a6c86c22204
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-07-15 23:49:57 -0400

    staticx: Error if output path is a directory
    Fixes #40

--- staticx/utils.py
+++ staticx/utils.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+from .errors import *
 
 def make_executable(path):
     mode = os.stat(path).st_mode
@@ -8,3 +10,8 @@ def make_executable(path):
 def get_symlink_target(path):
     dirpath = os.path.dirname(os.path.abspath(path))
     return os.path.join(dirpath, os.readlink(path))
+
+def move_file(src, dst):
+    if os.path.isdir(dst):
+        raise DirectoryExistsError(dst)
+    shutil.move(src, dst)
