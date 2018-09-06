53a508d49c2e31b5917c066824675a2c7ab3293e
Jonathon Reinhart <JonathonReinhart@users.noreply.github.com>
2018-09-06 18:35:47 -0400

    libtar: Use <sys/sysmacros.h> for makedev() and friends (#63)
    libtar/extract.c: In function 'tar_extract_blockdev':
        libtar/extract.c:440:13: error: In the GNU C Library, "makedev" is defined
         by <sys/sysmacros.h>. For historical compatibility, it is
         currently defined by <sys/types.h> as well, but we plan to
         remove this soon. To use "makedev", include <sys/sysmacros.h>
         directly. If you did not intend to use a system-defined macro
         "makedev", you should undefine it after including <sys/types.h>. [-Werror]
             makedev(devmaj, devmin)) == -1)
                     ^~~~~~~~~~~~~~~~~~~~~~~
    
    Fixes #62

--- libtar/encode.c
+++ libtar/encode.c
@@ -14,6 +14,7 @@
 #include <pwd.h>
 #include <grp.h>
 #include <sys/types.h>
+#include <sys/sysmacros.h>
 #include <string.h>
 #include <stdlib.h>
 #include "libtar.h"
--- libtar/extract.c
+++ libtar/extract.c
@@ -14,6 +14,7 @@
 #include <string.h>
 #include <sys/param.h>
 #include <sys/types.h>
+#include <sys/sysmacros.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <utime.h>
