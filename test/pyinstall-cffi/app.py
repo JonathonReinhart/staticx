afc02e84c8a51523d6b33a4530ef4e847929fac4
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2019-03-10 00:20:01 -0500

    Add pyinstall-cffi test
    This should hopefully shake out bugs related to Pyinstaller + RPATH

---
+++ test/pyinstall-cffi/app.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from cffi import FFI
+import os
+import sys
+
+def main():
+    ffi = FFI()
+    ffi.cdef("""
+      int printf(const char *, ...);
+    """)
+
+    if False:
+        # use a C compiler: verify the decl above is right
+        libc = ffi.verify()
+
+    libc = ffi.dlopen(None)
+
+    libc.printf(b"Hello, %s!\n",
+            ffi.new("char[]", b"world"),
+            )
+
+if __name__ == '__main__':
+    print("PID:", os.getpid())
+    print("MEIPASS:", getattr(sys, '_MEIPASS', '<not set>'))
+
+    main()
---
+++ test/pyinstall-cffi/run_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+outfile=./dist/app.staticx
+
+# Only run if PyInstaller is installed
+# By gracefully failing here, we can control which versions of Python this test
+# runs under in requirements.txt
+pyinstaller --version 2>/dev/null || { echo "PyInstaller not installed"; exit 0; }
+
+
+echo -e "\n\nTest StaticX against PyInstalled application"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Run the application normally
+echo -e "\nPython app run normally:"
+python app.py
+
+# Build a PyInstaller "onefile" application
+echo -e "\nBuilding PyInstaller 'onfile' application:"
+pyinstaller -F app.py
+
+# Run the PyInstalled application
+echo -e "\nPyInstalled application run:"
+./dist/app
+
+# Make a staticx executable from it
+echo -e "\nMaking staticx executable (\$STATICX_FLAGS=$STATICX_FLAGS):"
+staticx $STATICX_FLAGS ./dist/app $outfile
+
+# Run that executable
+echo -e "\nRunning staticx executable"
+$outfile
+
+# Run it under an old distro
+if [ -n "$TEST_DOCKER_IMAGE" ]; then
+    echo -e "\nRunning staticx executable under $TEST_DOCKER_IMAGE"
+    scuba --image $TEST_DOCKER_IMAGE $outfile
+fi
