JonathonReinhart/staticx

commit 2d31b3985b24b805d5ef178cf26303cbde36b2b5

staticx: Force program to be executable in archive
This fixes #109

staticx/utils.py
@@ -3,9 +3,14 @@ import errno
 import shutil
 from .errors import *
 
+def make_mode_executable(mode):
+    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    return mode
+
+
 def make_executable(path):
     mode = os.stat(path).st_mode
-    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    make_mode_executable(mode)
     os.chmod(path, mode)
 
 def get_symlink_target(path):

staticx/archive.py
@@ -11,7 +11,7 @@ except (ImportError, AttributeError):
     from backports import lzma
 
 from .bcjfilter import get_bcj_filter_arch
-from .utils import get_symlink_target
+from .utils import get_symlink_target, make_mode_executable
 from .constants import *
 from .errors import *
 
@@ -92,9 +92,13 @@ class SxArchive(object):
         The program will be added with a fixed name.
         Should only be called once. TODO: Enforce this.
         """
+        def make_exec(tarinfo):
+            tarinfo.mode = make_mode_executable(tarinfo.mode)
+            return tarinfo
+
         arcname = PROG_FILENAME
         logging.info("Adding {} as {}".format(path, arcname))
-        self.tar.add(path, arcname=arcname)
+        self.tar.add(path, arcname=arcname, filter=make_exec)
 
     def add_library(self, path):
         """Add a library to the archive

commit dce717025896a8107861290a3cc3b590e47d4a57

Fix ldd "you do not have execution..." warning
ldd: warning: you do not have execution permission for `/tmp/staticx-pyi-fe35tk1i/_asyncio.cpython-38-x86_64-linux-gnu.so'

This bug was introduced in 2d31b398.

staticx/utils.py
@@ -10,7 +10,7 @@ def make_mode_executable(mode):
 
 def make_executable(path):
     mode = os.stat(path).st_mode
-    make_mode_executable(mode)
+    mode = make_mode_executable(mode)
     os.chmod(path, mode)
 
 def get_symlink_target(path):

unittest/test_utils.py
@@ -1,0 +1,10 @@
+import tempfile
+import os
+
+from staticx import utils
+
+def test_make_executable():
+    with tempfile.NamedTemporaryFile() as tf:
+        assert (os.stat(tf.name).st_mode & 0o111) == 0
+        utils.make_executable(tf.name)
+        assert (os.stat(tf.name).st_mode & 0o111) != 0