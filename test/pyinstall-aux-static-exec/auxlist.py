cfd7a64fd670adce90197ca5679c36fe08194827
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-07-19 00:03:31 -0400

    test: Drop aux-*-dynamic from pyinstall-aux-static-exec test
    Bringing along your own extra dynamically-linked executable is not
    supported by staticx.
    
    Even the name of this overall test says "static".
    
    See:
    https://github.com/JonathonReinhart/staticx/issues/129#issuecomment-657246015

--- test/pyinstall-aux-static-exec/auxlist.py
+++ test/pyinstall-aux-static-exec/auxlist.py
@@ -1,6 +1,6 @@
+# This list originally had -dynamic variants, but that is not supported by staticx.
+# See https://github.com/JonathonReinhart/staticx/issues/129#issuecomment-657246015
 aux_apps = (
-    'aux-glibc-dynamic',
     'aux-glibc-static',
-    'aux-musl-dynamic',
     'aux-musl-static',
 )
