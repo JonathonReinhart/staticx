39e827bb41cc9770a9aab4d0e74150a8d468d767
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-07-24 21:28:59 -0400

    test: Update basic pyinstall app to improve test
    - Call getpwuid() to prove that we are safe from NSS
    - Call external 'id' program from pyinstall app to prove that we can
      safely exec with nssfix

--- test/pyinstall/app.py
+++ test/pyinstall/app.py
@@ -1,8 +1,13 @@
 import os
 import sys
+import pwd
+import subprocess
 
 def get_resource_dir():
     default = os.path.dirname(os.path.abspath(__file__))
     return getattr(sys, '_MEIPASS', default)
     
-print("Hello world! Resource dir: {}".format(get_resource_dir()))
+print("Hello {}! Resource dir: {}".format(
+    pwd.getpwuid(os.getuid()).pw_name,
+    get_resource_dir()),
+)
