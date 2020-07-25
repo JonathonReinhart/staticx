2586f1b986f20bb769323dc08e0476840ee5ea8e
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-07-25 18:14:11 -0400

    staticx: Add utils.coerce_sequence

--- unittest/test_utils.py
+++ unittest/test_utils.py
@@ -8,3 +8,30 @@ def test_make_executable():
         assert (os.stat(tf.name).st_mode & 0o111) == 0
         utils.make_executable(tf.name)
         assert (os.stat(tf.name).st_mode & 0o111) != 0
+
+# is_iterable
+def test_is_iterable_str():
+    assert not utils.is_iterable("foo")
+
+def test_is_iterable_list():
+    assert utils.is_iterable([1, 2, 3])
+
+def test_is_iterable_tuple():
+    assert utils.is_iterable((1, 2, 3))
+
+
+# coerce_sequence
+def test_coerce_sequence_scalar_input():
+    assert utils.coerce_sequence(42) == [42]
+    assert utils.coerce_sequence("foo") == ["foo"]
+
+def test_coerce_sequence_list_input():
+    assert utils.coerce_sequence([69, 420]) == [69, 420]
+    assert utils.coerce_sequence(["foo", "bar"]) == ["foo", "bar"]
+
+def test_coerce_sequence_tuple_input():
+    assert utils.coerce_sequence((69, 420)) == [69, 420]
+    assert utils.coerce_sequence(("foo", "bar")) == ["foo", "bar"]
+
+def test_coerce_sequence_tuple_output():
+    assert utils.coerce_sequence([69, 420], tuple) == (69, 420)
