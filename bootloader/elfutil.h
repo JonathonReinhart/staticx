5785d7ddc70b01e29619db99cfe2fe360c743235
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-09-27 19:48:24 -0400

    bootloader: Define Elf_* types for 32/64 bit

--- bootloader/elfutil.h
+++ bootloader/elfutil.h
@@ -4,10 +4,19 @@
 #include <elf.h>
 #include <stdbool.h>
 
-#define Elf_Ehdr    Elf64_Ehdr
-#define Elf_Phdr    Elf64_Phdr
-#define Elf_Shdr    Elf64_Shdr
-#define Elf_Dyn     Elf64_Dyn
+#if __WORDSIZE == 64
+# define Elf_Ehdr    Elf64_Ehdr
+# define Elf_Phdr    Elf64_Phdr
+# define Elf_Shdr    Elf64_Shdr
+# define Elf_Dyn     Elf64_Dyn
+#elif __WORDSIZE == 32
+# define Elf_Ehdr    Elf32_Ehdr
+# define Elf_Phdr    Elf32_Phdr
+# define Elf_Shdr    Elf32_Shdr
+# define Elf_Dyn     Elf32_Dyn
+#else /* __WORDSIZE */
+# error __WORDSIZE is not valid
+#endif /* __WORDSIZE */
 
 bool
 elf_is_valid(const Elf_Ehdr *ehdr);
