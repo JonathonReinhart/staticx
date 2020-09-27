9618cf37d3603b91af9ac6c214c4df864f218d70
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-09-27 19:48:24 -0400

    bootloader: Define Elf_* types for 32/64 bit

--- bootloader/elfutil.h
+++ bootloader/elfutil.h
@@ -3,11 +3,28 @@
 
 #include <elf.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#define Elf_Ehdr    Elf64_Ehdr
-#define Elf_Phdr    Elf64_Phdr
-#define Elf_Shdr    Elf64_Shdr
-#define Elf_Dyn     Elf64_Dyn
+/**
+ * Determine native ELF type by looking at size of pointer.
+ * This should work even for x32 which uses elf32:
+ * https://stackoverflow.com/a/36347228
+ */
+#if UINTPTR_MAX == UINT64_MAX
+/* 64-bit */
+# define Elf_Ehdr    Elf64_Ehdr
+# define Elf_Phdr    Elf64_Phdr
+# define Elf_Shdr    Elf64_Shdr
+# define Elf_Dyn     Elf64_Dyn
+#elif UINTPTR_MAX == UINT32_MAX
+/* 32-bit */
+# define Elf_Ehdr    Elf32_Ehdr
+# define Elf_Phdr    Elf32_Phdr
+# define Elf_Shdr    Elf32_Shdr
+# define Elf_Dyn     Elf32_Dyn
+#else
+# error unexpected UINTPTR_MAX
+#endif
 
 bool
 elf_is_valid(const Elf_Ehdr *ehdr);
