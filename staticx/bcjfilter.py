87379a49ab5fa5d24dd327d5087cad09511c42b0
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-10-03 00:25:08 -0400

    Refactor bcjfilter to centralize logic and simplify consumers

--- staticx/bcjfilter.py
+++ staticx/bcjfilter.py
@@ -1,38 +1,65 @@
 import platform
+import lzma
 
 # NOTE: This is also used by libxz/SConscript
 
-def get_bcj_filter_arch():
-    """
-    Get an appropriate BCJ filter for the current architecture.
+class BCJFilter:
+    __slots__ = ['arch']
 
-    Returns just the architecture part of the BCJ filter name.
-    This can be prepended with FILTER_ for a Python lzma module constant,
-    or XZ_DEC_ for an XZ Embedded decoder macro.
-    """
-    # It's possible that the code being compressed might not match that of the
-    # current Python interpreter. In that case the problem becomes much more
-    # complicated, as we have to inspect the files to make a determination.
-    # This approach should be "good enough"; the worst case scenario is only a
-    # slightly worse compression ratio.
+    def __init__(self, arch):
+        self.arch = arch
 
-    machine = platform.machine()
+    def __repr__(self):
+        return 'BCJFilter({})'.format(self.arch)
 
-    if machine in ('i386', 'i686', 'x86_64'):
-        return 'X86'
+    def __str__(self):
+        return self.arch
 
-    if machine == 'ia64':
-        return 'IA64'
+    @property
+    def xz_dec_macro(self):
+        """Get the XZ Embedded decoder macro name for this filter"""
+        return 'XZ_DEC_' + self.arch
 
-    if machine.startswith('arm'):   # arm, armv8b, etc.
-        return 'ARM'
+    @property
+    def lzma_filter_id(self):
+        """Get a Python lzma module filter id for this filter
 
-    # TODO: 'ARMTHUMB'
+        See https://docs.python.org/3/library/lzma.html#filter-chain-specs
+        """
+        return getattr(lzma, 'FILTER_' + self.arch)
 
-    if machine.startswith('ppc'):
-        return 'POWERPC'
+    @classmethod
+    def for_arch(cls, arch):
+        """
+        Get an appropriate BCJ filter for the given architecture.
+        """
+        if arch in ('i386', 'i686', 'x86_64'):
+            return cls('X86')
 
-    if machine.startswith('sparc'):
-        return 'SPARC'
+        if arch == 'ia64':
+            return cls('IA64')
 
-    return None
+        if arch.startswith('arm'):   # arm, armv8b, etc.
+            return cls('ARM')
+
+        # TODO: 'ARMTHUMB'
+
+        if arch.startswith('ppc'):
+            return cls('POWERPC')
+
+        if arch.startswith('sparc'):
+            return cls('SPARC')
+
+        return None
+
+    @classmethod
+    def for_current_arch(cls):
+        """
+        Get an appropriate BCJ filter for the current architecture.
+        """
+        # It's possible that the code being compressed might not match that of the
+        # current Python interpreter. In that case the problem becomes much more
+        # complicated, as we have to inspect the files to make a determination.
+        # This approach should be "good enough"; the worst case scenario is only a
+        # slightly worse compression ratio.
+        return cls.for_arch(platform.machine())
