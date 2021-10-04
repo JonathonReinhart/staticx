7ff113df9ab8e125d4c9e1b55ab751a365020e5f
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-10-03 21:15:33 -0400

    test: Add test to verify staticx catches pyinstalled libs with RUNPATH

---
+++ test/pyinstall-lib-runpath/run_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+echo -e "\n\nVerify StaticX rejects PyInstalled application with library using RUNPATH"
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+source ../funcs.sh
+
+app="./dist/app"
+outfile="./dist/app.staticx"
+
+# Build the shared library
+gcc -Wall -Werror -fPIC -c -o shlib.o shlib.c
+gcc -shared -Wl,-rpath=/bogus/absolute/path -Wl,--enable-new-dtags -o shlib.so shlib.o
+verify_uses_runpath shlib.so
+
+# Run the application normally
+echo -e "\nPython app run normally:"
+python3 app.py
+
+# Build a PyInstaller "onefile" application
+echo -e "\nBuilding PyInstaller 'one-file' application:"
+pyinstaller -F app.spec
+
+# Run the PyInstalled application
+echo -e "\nPyInstalled application run:"
+$app
+
+# Make a staticx executable from it
+# This is an expected failure!
+echo -e "\nMaking staticx executable (\$STATICX_FLAGS=$STATICX_FLAGS) [EXPECTED FAILURE]"
+
+if output=$(staticx $STATICX_FLAGS $app $outfile); then
+    echo "FAIL: Staticx permitted a problematic library using RUNPATH"
+    exit 66
+fi
+if ! (echo "$output" | grep -q "unsupported DT_RUNPATH"); then
+    echo "FAIL: Unexpected output text:"
+    echo "$output"
+    exit 66
+fi
+echo "Success"
---
+++ test/pyinstall-lib-runpath/shlib.h
@@ -0,0 +1,6 @@
+#ifndef SHLIB_DOT_H
+#define SHLIB_DOT_H
+
+int foo(void);
+
+#endif /* SHLIB_DOT_H */
