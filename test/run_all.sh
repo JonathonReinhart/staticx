0e22223ed08bea9406ef69d96c35c7d3422a46b9
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2021-10-22 16:46:25 -0400

    test: Add pyinstall-lib-runpath to run_all.sh
    This was missed in 7ff113df9ab8.

--- test/run_all.sh
+++ test/run_all.sh
@@ -21,6 +21,9 @@ pyinstall-cffi/run_test.sh
 # Run test against app built with PyInstaller and includes a helper app
 pyinstall-aux-static-exec/run_test.sh
 
+# Test against a PyInstaller app that uses unsupported RUNPATH
+pyinstall-lib-runpath/run_test.sh
+
 # Verify that target system libs can't be loaded
 no-default-lib/run_test.sh
 
