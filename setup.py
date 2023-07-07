7ba10fe5c6dc9c360aa6cd264a677963c5af43f6
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2023-07-07 04:30:11 -0400

    Add support for Python 3.8 through 3.11

--- setup.py
+++ setup.py
@@ -135,8 +135,11 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Build Tools',
     ],
     license = 'GPL v2 with special exception allowing StaticX to build and'
