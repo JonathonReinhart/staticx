2459ce2b3082b3116d9ccd1edd3008a5df708254
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2023-07-09 01:01:14 -0400

    staticx: Update test/**/*.py to use f-strings

--- test/pyinstall/app.py
+++ test/pyinstall/app.py
@@ -7,7 +7,5 @@ def get_resource_dir():
     default = os.path.dirname(os.path.abspath(__file__))
     return getattr(sys, '_MEIPASS', default)
     
-print("Hello {}! Resource dir: {}".format(
-    pwd.getpwuid(os.getuid()).pw_name,
-    get_resource_dir()),
-)
+print(f"Hello {pwd.getpwuid(os.getuid()).pw_name}!")
+print(f"Resource dir: {get_resource_dir()}")
