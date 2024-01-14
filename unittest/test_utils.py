a5e1e92b811e5e34bbb9fa937005b3694bee1157
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2024-01-14 10:47:59 -0500

    staticx: Add type info to staticx.utils
    This also updates the single() implementation, replacing the use of
    _SENTINEL as a nothing-to-return marker with a separate bool variable.
    
    This also simplifies coerce_sequence() to always return a list, removing
    the return-type argument.

--- unittest/test_utils.py
+++ unittest/test_utils.py
@@ -35,9 +35,6 @@ def test_coerce_sequence_tuple_input():
     assert utils.coerce_sequence((69, 420)) == [69, 420]
     assert utils.coerce_sequence(("foo", "bar")) == ["foo", "bar"]
 
-def test_coerce_sequence_tuple_output():
-    assert utils.coerce_sequence([69, 420], tuple) == (69, 420)
-
 
 # single
 def test_single_success():
