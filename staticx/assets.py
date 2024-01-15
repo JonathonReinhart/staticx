6fc5dbcecfb29a000d3074de8fe163baf60b7f00
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2024-01-14 23:15:40 -0500

    staticx: Add type info to staticx.assets

--- staticx/assets.py
+++ staticx/assets.py
@@ -1,9 +1,11 @@
 import importlib.resources
 import sys
+from tempfile import _TemporaryFileWrapper
+from typing import Any, IO
 from .utils import copy_fileobj_to_tempfile
 
 
-def locate_asset(name, debug):
+def locate_asset(name: str, debug: bool) -> IO[bytes]:
     mode = 'debug' if debug else 'release'
     path = '/'.join(('assets', mode, name))
     try:
@@ -12,6 +14,6 @@ def locate_asset(name, debug):
         raise KeyError(f"Asset not found: {name!r} (mode={mode!r})")
 
 
-def copy_asset_to_tempfile(assetname, debug, **kwargs):
+def copy_asset_to_tempfile(assetname: str, debug: bool, **kwargs: Any) -> _TemporaryFileWrapper:
     with locate_asset(assetname, debug) as fsrc:
         return copy_fileobj_to_tempfile(fsrc, **kwargs)
