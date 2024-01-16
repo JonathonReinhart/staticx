4ffa0b2a0aa555c65d398984df0224819724c34f
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2024-01-15 19:11:29 -0500

    staticx: Add type info to staticx.hooks (__init__.py)

--- staticx/hooks/__init__.py
+++ staticx/hooks/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import annotations
 from .pyinstaller import process_pyinstaller_archive
 from .glibc import process_glibc_prog
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..api import StaticxGenerator
 
 hooks = [
     process_pyinstaller_archive,
@@ -7,6 +11,6 @@ hooks = [
 ]
 
 
-def run_hooks(sx):
+def run_hooks(sx: StaticxGenerator) -> None:
     for hook in hooks:
         hook(sx)
