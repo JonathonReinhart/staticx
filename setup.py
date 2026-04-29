8f744a83332f335ec28c6d657104efe2c4c49439
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2026-04-29 00:48:54 -0400

    setup.py: Reformat with black

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ import os
 import sys
 from subprocess import check_call
 
-
 ################################################################################
 # Dynamic versioning
+
 
 def get_dynamic_version():
     import staticx.version
@@ -18,7 +18,7 @@ def get_dynamic_version():
     # If CI_VERSION_BUILD_NUMBER is set, append that to the base version
     build_num = os.getenv("CI_VERSION_BUILD_NUMBER")
     if build_num:
-        return f'{staticx.version.BASE_VERSION}.{build_num}'
+        return f"{staticx.version.BASE_VERSION}.{build_num}"
 
     # Otherwise, use the auto-versioning
     return staticx.version.__version__
@@ -27,38 +27,42 @@ def get_dynamic_version():
 ################################################################################
 # Commands / hooks
 
+
 class build_bootloader(Command):
     description = "Build staticx bootloader binary"
 
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
 
     def run(self):
         args = [
-            'scons',
-            '-Q',       # Quiet output (except for build status)
-            f'STATICX_VERSION={get_dynamic_version()}',
+            "scons",
+            "-Q",  # Quiet output (except for build status)
+            f"STATICX_VERSION={get_dynamic_version()}",
         ]
         check_call(args)
 
 
 class build_hook(build):
     def run(self):
-        self.run_command('build_bootloader')
+        self.run_command("build_bootloader")
         build.run(self)
 
 
 class bdist_wheel_hook(bdist_wheel):
     def finalize_options(self):
-        self.plat_name = get_platform()     # Equivalent to passing --plat-name
+        self.plat_name = get_platform()  # Equivalent to passing --plat-name
         super().finalize_options()
 
 
 class UnsupportedPlatformError(Exception):
     pass
+
 
 def get_platform():
     """Get platform string (e.g. "manylinux1_x86_64")
@@ -89,8 +93,8 @@ def get_platform():
     uname = os.uname()
 
     # Apply the same conversions as distutils.util.get_platform()
-    osname = uname.sysname.lower().replace('/', '')
-    machine = uname.machine.replace(' ', '_').replace('/', '-')
+    osname = uname.sysname.lower().replace("/", "")
+    machine = uname.machine.replace(" ", "_").replace("/", "-")
 
     # Check compatibility
     if osname != "linux":
@@ -106,17 +110,18 @@ def get_platform():
     # See:
     #   https://github.com/pypa/pip/pull/3497#discussion_r54870308
     #   https://github.com/pypa/wheel/commit/b9dbb6fa257c
-    if machine == "x86_64" and sys.maxsize == 0x7fffffff:
+    if machine == "x86_64" and sys.maxsize == 0x7FFFFFFF:
         machine = "i686"
 
-    return f'manylinux1_{machine}'
+    return f"manylinux1_{machine}"
+
 
 ################################################################################
 
 setup(
     #####
     # Dynamic core metadata, in addition to pyproject.toml [project]
-    version = get_dynamic_version(),
+    version=get_dynamic_version(),
     #####
     # Setuptools-specific config
     # We could put this in pyproject.toml [tool.setuptools], but choose not to:
@@ -124,17 +129,17 @@ setup(
     # - We need setup.py to provide build_hook, so we might as well keep all
     #   setuptools-specific config here, in one file.
     packages=find_packages(),
-    package_data = {
-        'staticx': ['assets/*/*'],
+    package_data={
+        "staticx": ["assets/*/*"],
     },
     include_package_data=True,  # https://github.com/pypa/setuptools/issues/1064
-    zip_safe = False,   # http://stackoverflow.com/q/24642788/119527
+    zip_safe=False,  # http://stackoverflow.com/q/24642788/119527
     # http://stackoverflow.com/questions/17806485
     # http://stackoverflow.com/questions/21915469
     # PyInstaller setup.py
-    cmdclass = {
-        'build_bootloader': build_bootloader,
-        'build':            build_hook,
-        'bdist_wheel':      bdist_wheel_hook,
+    cmdclass={
+        "build_bootloader": build_bootloader,
+        "build": build_hook,
+        "bdist_wheel": bdist_wheel_hook,
     },
 )
