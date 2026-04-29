8c1ead74dd199e2b1a44deb692094cd3960eee1f
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2026-04-29 01:25:10 -0400

    test: Reformat with black

--- test/pyinstall-lib-runpath/app.py
+++ test/pyinstall-lib-runpath/app.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 mydir = Path(__file__).parent
 
-libfoo_path = (mydir / 'libfoo.so').absolute()
-libbar_path = (mydir / 'libbar.so').absolute()
+libfoo_path = (mydir / "libfoo.so").absolute()
+libbar_path = (mydir / "libbar.so").absolute()
 
 assert libfoo_path.exists()
 assert libbar_path.exists()
@@ -13,9 +13,11 @@ assert libbar_path.exists()
 libfoo = CDLL(libfoo_path)
 libbar = CDLL(libbar_path)
 
+
 def setup_prototype(func, restype, *argtypes):
     func.restype = restype
     func.argtypes = argtypes
+
 
 setup_prototype(libfoo.foo, c_int)
 setup_prototype(libbar.bar, c_int)
