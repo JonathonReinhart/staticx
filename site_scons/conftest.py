44a9c34c1007657d83ef188365ec832b75782bb4
Jonathon Reinhart <jonathon.reinhart@gmail.com>
2026-04-29 01:41:12 -0400

    site_scons: Reformat with black

--- site_scons/conftest.py
+++ site_scons/conftest.py
@@ -1,7 +1,7 @@
 def CheckNSS(context):
-    context.Message('Checking for NSS...')
+    context.Message("Checking for NSS...")
     ok, output = context.TryRun(
-        extension='.c',
+        extension=".c",
         text="""
 #include <nss.h>
 int main(void)
@@ -9,11 +9,12 @@ int main(void)
     __nss_configure_lookup("passwd", "files");
     return 0;
 }
-""")
+""",
+    )
     context.Result(ok)
     return ok
 
 
 custom_tests = dict(
-    CheckNSS = CheckNSS,
+    CheckNSS=CheckNSS,
 )
